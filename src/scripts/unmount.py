8a473f73a1e5a63de854f7cb37e84f02638f1190
fairy26 <y-yoshida@ai-ms.com>
2021-10-14 15:03:28 +0900

    Black format

--- src/scripts/unmount.py
+++ src/scripts/unmount.py
@@ -4,39 +4,39 @@ import sys
 
 
 def _add_permission(cmd):
-  cmd.insert(0, 'sudo')
+    cmd.insert(0, "sudo")
 
 
 def unmount(disk):
 
-  authorized = True  # False: add 'sudo' to command
-  if os.geteuid():
-    # args = [sys.executable] + sys.argv
-    # os.execlp('sudo', *args)
-    authorized = False
+    authorized = True  # False: add 'sudo' to command
+    if os.geteuid():
+        # args = [sys.executable] + sys.argv
+        # os.execlp('sudo', *args)
+        authorized = False
 
-  if not os.path.exists(disk):
-    sys.exit(-1)
+    if not os.path.exists(disk):
+        sys.exit(-1)
 
-  realpath = os.path.realpath(disk)
-  
-  target = realpath
-  with open('/proc/mounts', 'r', encoding='utf-8') as f:
-    for raw in reversed(f.readlines()):
-      out = raw.split(' ')
-      device_name = out[0]
-      mountpoint = out[1]
-      if target == mountpoint:
-        cmd = ['umount', target]
-        if not authorized:
-          _add_permission(cmd)
-        subprocess.run(cmd, check=True)
-        if os.path.exists(target):
-          # os.rmdir(target)
-          cmd_rmdir = ['rmdir', target]
-          if not authorized:
-            _add_permission(cmd_rmdir)
-          subprocess.run(cmd_rmdir)
-        target = device_name
-        
-  return 'not_mounted'
+    realpath = os.path.realpath(disk)
+
+    target = realpath
+    with open("/proc/mounts", "r", encoding="utf-8") as f:
+        for raw in reversed(f.readlines()):
+            out = raw.split(" ")
+            device_name = out[0]
+            mountpoint = out[1]
+            if target == mountpoint:
+                cmd = ["umount", target]
+                if not authorized:
+                    _add_permission(cmd)
+                subprocess.run(cmd, check=True)
+                if os.path.exists(target):
+                    # os.rmdir(target)
+                    cmd_rmdir = ["rmdir", target]
+                    if not authorized:
+                        _add_permission(cmd_rmdir)
+                    subprocess.run(cmd_rmdir)
+                target = device_name
+
+    return "not_mounted"
