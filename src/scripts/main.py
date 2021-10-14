8f81b6bc8ed8ff2e26fdbab13b56952e33c7314d
fairy26 <y-yoshida@ai-ms.com>
2021-10-14 15:10:09 +0900

    Revert "Black format"
    This reverts commit 8a473f73a1e5a63de854f7cb37e84f02638f1190.

--- src/scripts/main.py
+++ src/scripts/main.py
@@ -4,7 +4,6 @@ import sys
 from check import get_hdd_lists
 from mount import mount
 from unmount import unmount
-
 # from copy import copy
 
 
@@ -13,54 +12,54 @@ def send(message, file=sys.stdout):
     file.flush()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    # if os.geteuid():
-    #   argv = sys.argv + ['--uid']
-    #   args = [sys.executable] + argv
-    #   os.execlp('sudo', *args)
+  # if os.geteuid():
+  #   argv = sys.argv + ['--uid']
+  #   args = [sys.executable] + argv
+  #   os.execlp('sudo', *args)
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--path", default=None, action="extend", nargs="+", type=str)
-    parser.add_argument("--mount", action="store_true")
-    parser.add_argument("--ro", "--read-only", action="store_true")
-    parser.add_argument("--unmount", action="store_true")
-    parser.add_argument("--check", action="store_true")
-    parser.add_argument("--copy", action="store_true")
-    args = parser.parse_args()
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--path', default=None, action="extend", nargs="+", type=str)
+  parser.add_argument('--mount', action='store_true')
+  parser.add_argument('--ro', '--read-only', action='store_true')
+  parser.add_argument('--unmount', action='store_true')
+  parser.add_argument('--check', action='store_true')
+  parser.add_argument('--copy', action='store_true')
+  args = parser.parse_args()
 
-    if args.mount:
-        send("mount")
+  if args.mount:
+    send('mount')
 
-        status = mount(args.path[0], args.ro)
-        send(status)
+    status = mount(args.path[0], args.ro)
+    send(status)
 
-    if args.unmount:
-        send("unmount")
+  if args.unmount:
+    send('unmount')
 
-        status = unmount(args.path[0])
-        send(status)
+    status = unmount(args.path[0])
+    send(status)
 
-    if args.check:
-        send("check")
+  if args.check:
+    send('check')
 
-        hdd_list, mounted_list, access_list = get_hdd_lists()
-        send(hdd_list)
-        send(mounted_list)
-        send(access_list)
+    hdd_list, mounted_list, access_list = get_hdd_lists()
+    send(hdd_list)
+    send(mounted_list)
+    send(access_list)
 
-    if args.copy:
-        send("copy")
+  if args.copy:
+    send("copy")
 
-        # copy() を実行
+    # copy() を実行
 
-        # 以下, 簡単なpbar表示
-        from time import sleep
-        from tqdm import tqdm
+    # 以下, 簡単なpbar表示
+    from time import sleep
+    from tqdm import tqdm
 
-        sec = range(40)
-        bar_format = "{percentage:.0f}, {remaining}\n"
-        with tqdm(total=len(sec), bar_format=bar_format, leave=None) as pbar:
-            for i in sec:
-                pbar.update(1)
-                sleep(0.2)
+    sec = range(40)
+    bar_format = '{percentage:.0f}, {remaining}\n'
+    with tqdm(total=len(sec), bar_format=bar_format, leave=None) as pbar:
+      for i in sec:
+        pbar.update(1)
+        sleep(0.2)
--- src/scripts/mount.py
+++ src/scripts/mount.py
@@ -5,81 +5,72 @@ import sys
 
 
 def _add_permission(cmd):
-    cmd.insert(0, "sudo")
+  cmd.insert(0, 'sudo')
 
 
 def mount(disk, ro=False):
-    MAPPING_UID = 1000
-    MAPPING_GID = 1000
-    uid = gid = str(os.geteuid())
+  MAPPING_UID = 1000
+  MAPPING_GID = 1000
+  uid = gid = str(os.geteuid())
 
-    # TODO: command not found に対処する
-    # -> 仮想環境を作成してもできなかった
+  # TODO: command not found に対処する
+  # -> 仮想環境を作成してもできなかった
 
-    authorized = True  # False: add 'sudo' to command
-    if os.geteuid():
-        # args = [sys.executable] + sys.argv
-        # os.execlp('sudo', *args)
-        authorized = False
+  authorized = True  # False: add 'sudo' to command
+  if os.geteuid():
+    # args = [sys.executable] + sys.argv
+    # os.execlp('sudo', *args)
+    authorized = False
 
-    if not os.path.exists(disk):
-        return "not_mounted"
+  if not os.path.exists(disk):
+    return 'not_mounted'
 
-    realpath = os.path.realpath(disk)
+  realpath = os.path.realpath(disk)
 
-    m = re.match(r"/dev/(sd.+)", realpath)
-    if not m:
-        return "not_mounted"
+  m = re.match(r'/dev/(sd.+)', realpath)
+  if not m:
+    return 'not_mounted'  
 
-    device_name = m.group(1)
-    raw_disk_mount_path = os.path.join("/mnt", f"raw_{device_name}")
-    disk_mount_path = os.path.join("/mnt", f"{device_name}")
+  device_name = m.group(1)
+  raw_disk_mount_path = os.path.join('/mnt', f'raw_{device_name}')
+  disk_mount_path = os.path.join('/mnt', f'{device_name}')
 
-    raw_disk_is_mounted = False
-    disk_is_mounted = False
-    with open("/proc/mounts", "r", encoding="utf-8") as f:
-        for raw in f.readlines():
-            device_name = raw.split(" ")[0]
-            if realpath == device_name:
-                raw_disk_is_mounted = True
-            if raw_disk_mount_path == device_name:
-                disk_is_mounted = True
+  raw_disk_is_mounted = False
+  disk_is_mounted = False
+  with open('/proc/mounts', 'r', encoding='utf-8') as f:
+    for raw in f.readlines():
+      device_name = raw.split(' ')[0]
+      if realpath == device_name:
+        raw_disk_is_mounted = True
+      if raw_disk_mount_path == device_name:
+        disk_is_mounted = True
 
-    if not raw_disk_is_mounted:
-        # os.makedirs(raw_disk_mount_path, exist_ok=True)
-        cmd_mkdir = ["mkdir", "-p", raw_disk_mount_path]
-        if not authorized:
-            _add_permission(cmd_mkdir)
-        subprocess.run(cmd_mkdir)
+  if not raw_disk_is_mounted:
+    # os.makedirs(raw_disk_mount_path, exist_ok=True)
+    cmd_mkdir = ['mkdir', '-p', raw_disk_mount_path]
+    if not authorized:
+      _add_permission(cmd_mkdir)
+    subprocess.run(cmd_mkdir)
 
-        cmd = ["mount"]
-        if not authorized:
-            _add_permission(cmd)
-        if ro:
-            cmd.extend(["-o", "ro"])
-        cmd.extend([realpath, raw_disk_mount_path])
-        subprocess.run(cmd, check=True)
-        # print(cmd)
+    cmd = ['mount']
+    if not authorized:
+      _add_permission(cmd)
+    if ro:
+      cmd.extend(['-o', 'ro'])
+    cmd.extend([realpath, raw_disk_mount_path])
+    subprocess.run(cmd, check=True)
+    # print(cmd)
 
-    if not disk_is_mounted:
-        # os.makedirs(disk_mount_path, exist_ok=True)
-        cmd_mkdir = ["mkdir", "-p", disk_mount_path]
-        if not authorized:
-            _add_permission(cmd_mkdir)
-        subprocess.run(cmd_mkdir)
+  if not disk_is_mounted:
+    # os.makedirs(disk_mount_path, exist_ok=True)
+    cmd_mkdir = ['mkdir', '-p', disk_mount_path]
+    if not authorized:
+      _add_permission(cmd_mkdir)
+    subprocess.run(cmd_mkdir)
+    
+    cmd = ['bindfs', '-u', str(uid), '-g', str(gid), f'--map={MAPPING_UID}/{uid}:@{MAPPING_GID}/@{gid}', raw_disk_mount_path, disk_mount_path]
+    if not authorized:
+      _add_permission(cmd)
+    subprocess.run(cmd, check=True)
 
-        cmd = [
-            "bindfs",
-            "-u",
-            str(uid),
-            "-g",
-            str(gid),
-            f"--map={MAPPING_UID}/{uid}:@{MAPPING_GID}/@{gid}",
-            raw_disk_mount_path,
-            disk_mount_path,
-        ]
-        if not authorized:
-            _add_permission(cmd)
-        subprocess.run(cmd, check=True)
-
-    return disk_mount_path
+  return disk_mount_path
